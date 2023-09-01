609db3bd0db7b10ee7f88b87a6a9a6b47dd0b59a
y0usSs3F <120474463+y0usSs3F@users.noreply.github.com>
2023-09-01 00:10:05 -0400

    Update Read_Screenshots.sh

--- Read_Screenshots_In_Browser/Read_Screenshots.sh
+++ Read_Screenshots_In_Browser/Read_Screenshots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# The Script is basically you should be at the directory of the screenshots were taken and then the ls command would take the name of them and then ...
+# The Script is basically you should be at the directory of the screenshots were taken and then the ls command would take the name of them and then create index.html file that contains those names of the picture with the picture under it, so it's a html nice file to open it on the browser and fast and ease your work during check the screenshots that tooks from specific target
 
 
 for i in $(ls)
