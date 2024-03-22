84f75b693bf31f198611329595d71c112cd647c4
y0usSs3F <120474463+y0usSs3F@users.noreply.github.com>
2024-03-22 03:43:51 -0400

    Create oneLinerCommandTerminal.sh

---
+++ OneLiner/oneLinerCommandTerminal.sh
@@ -0,0 +1,7 @@
+
+# Convert Domains to IPs:
+for domain in $(cat all_subs.txt); do echo "Domain: $domain, IP: $(dig +short $domain | head -n 1)"; done | tee result_IPs.txt
+
+# Convert IPs to Domains:
+for ip in $(cut -d ':' -f 1 mongo_ips); do domain=$(dig +short -x $ip); [ -n "$domain" ] && echo "IP: $ip, Domain: $domain"; done | tee result_Domains.txt
+
