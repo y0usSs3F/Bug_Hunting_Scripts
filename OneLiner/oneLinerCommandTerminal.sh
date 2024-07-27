0322b49e0e604d44ffade8269800d14937f29139
y0usSs3F <120474463+y0usSs3F@users.noreply.github.com>
2024-07-27 07:24:23 -0400

    Update oneLinerCommandTerminal.sh

--- OneLiner/oneLinerCommandTerminal.sh
+++ OneLiner/oneLinerCommandTerminal.sh
@@ -1,6 +1,8 @@
 # Convert Domains to IPs:
-for domain in $(cat all_subs.txt); do echo "Domain: $domain, IP: $(dig +short $domain)"; done | tee result_IPs.txt
+└─$ for domain in $(cat all_subs.txt); do echo "Domain: $domain, IP: $(dig +short $domain)"; done | tee result_IPs.txt
 
 # Convert IPs to Domains:
-for ip in $(cut -d ':' -f 1 mongo_ips); do domain=$(dig +short -x $ip); [ -n "$domain" ] && echo "IP: $ip, Domain: $domain"; done | tee result_Domains.txt
+└─$ for ip in $(cut -d ':' -f 1 mongo_ips); do domain=$(dig +short -x $ip); [ -n "$domain" ] && echo "IP: $ip, Domain: $domain"; done | tee result_Domains.txt
 
+# Collect IPs from the result_IPs.txt file:
+└─$ grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' result_IPs.tx | sort -u | tee IPs.txt
